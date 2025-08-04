30ff87ea3cec839cb175b2dcb36143098f0822c9
Mariel de la Garza <mariel.code@gmail.com>
2025-08-04 12:23:10 -0400

    Update folder and tab styles

--- src/lib/components/Folder.svelte
+++ src/lib/components/Folder.svelte
@@ -17,36 +17,38 @@
 </div>
 
 <style>
-.folder {
-  position: relative;
-  width: 45%;
-  height: 100%;
-  background-image: url("/src/assets/light_wood.jpeg");
-  background-size: cover;
-  background-position: center;
-  background-color: beige;
-  z-index: 2;
-}
+  .folder {
+    position: relative;
+    width: 40%;
+    height: 100%;
+    background: linear-gradient(to left, #fdfadd 80%, #cfc7a0 100%);
+    z-index: 2;
+  }
 
-/* Tab bar with horizontal tabs */
-.tab-bar {
-  position: absolute;
-  right: -2.1rem; /* offset to the right of folder */
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
+  /* Tab bar with horizontal tabs */
+  .tab-bar {
+    position: absolute;
+    right: -2.1rem; /* offset to the right of folder */
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
 
-/* Individual tabs */
-.tab {
-  writing-mode: vertical-rl;
-  background: #fdfadd;
-  border-radius: 0 0.5rem 0.5rem 0;
-  padding: 1rem 0.5rem;
-  font-size: 1rem;
-  font-weight: bold;
-  cursor: pointer;
-  text-align: center;
-}
+  /* Individual tabs */
+  .tab {
+    writing-mode: vertical-rl;
+    background: #fdfadd;
+    border-radius: 0 0.5rem 0.5rem 0;
+    padding: 1rem 0.5rem;
+    font-size: 1rem;
+    font-weight: bold;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  /* Apply shadow to all tabs except the first one */
+  .tab:not(:first-child) {
+    background: linear-gradient(to left, #fdfadd 80%, #cfc7a0 100%);
+  }
 </style>
